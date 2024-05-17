edc6230d1142c9e7d6d43e1e6314fa4af06de9d6
qjlxg <qjlxg@163.com>
2024-05-17 10:49:51 +0800

    Update splitter.py

--- python/splitter.py
+++ python/splitter.py
@@ -1,7 +1,7 @@
 import requests
 
 # Fetch the content of the URL
-url = "https://raw.githubusercontent.com/Surfboardv2ray/TGParse/main/configtg.txt"
+url = "https://raw.githubusercontent.com/qjlxg/hy2/main/configtg.txt"
 
 try:
     response = requests.get(url)
